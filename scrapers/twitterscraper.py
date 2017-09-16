860882d774f0c19bb0a6c6bb8e486fcec39035c3
Unknown <kennethgabrielsen@hotmail.com>
2017-09-16 02:20:51 +0200

    Pushed twitterscraper( WIP)

---
+++ scrapers/twitterscraper.py
@@ -0,0 +1,39 @@
+import tweepy
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+import tweepy
+from tweepy import OAuthHandler
+
+consumer_key = 'YOUR-CONSUMER-KEY'
+consumer_secret = 'YOUR-CONSUMER-SECRET'
+access_token = 'YOUR-ACCESS-TOKEN'
+access_secret = 'YOUR-ACCESS-SECRET'
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+
+class MyListener(StreamListener):
+    def on_data(self, data):
+        try:
+            with open('python.json', 'a') as f:
+                f.write(data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+
+    def on_error(self, status):
+        print(status)
+        return True
+
+
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=['#bitcoin'])
+
+
+
